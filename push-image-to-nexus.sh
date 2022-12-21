a19cd7091ab255d3650968cff3e54fd174f217c7
Gangadhar <gangadhar.k@CELSYS217-BLR.CELSYSWTC.IN>
2022-12-21 09:58:48 +0530

    deploy-yes

--- push-image-to-nexus.sh
+++ push-image-to-nexus.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+ 
+#
+#  Build & upload docker image to Nexus
+#
+#
+#  Validate that this is a valid branch for CI/CD
+#
+
+
 export BRANCH_NAME=`echo "${TRAVIS_BRANCH}" | tr '[:upper:]' '[:lower:]'`
 case "${BRANCH_NAME}" in
         dev*) echo "Branch ${TRAVIS_BRANCH} is eligible for CI/CD" ;;
@@ -14,22 +23,50 @@ esac
 
 echo $TRAVIS_BRANCH
 echo ${DEPLOYMENT_SERVER}
+
+# get the short commit ID to use it as docker image tag
 export SHORT_COMMIT=`git rev-parse --short=7 ${TRAVIS_COMMIT}`
 echo "short commit $SHORT_COMMIT"
+
 sudo apt-get update
 sudo apt-get install -y jq
-IMAGE_NAME=`echo "${BUILD_REPO_NAME}_${TRAVIS_BRANCH}" | tr '[:upper:]' '[:lower:]'`
+
+#IMAGE_NAME=`echo "${BUILD_REPO_NAME}_${TRAVIS_BRANCH}" | tr '[:upper:]' '[:lower:]'`
+DJANGO_IMAGE_NAME=`echo "${BUILD_REPO_NAME}_${TRAVIS_BRANCH}_${DJANGO_CONTAINER_SERVICE_NAME}" | tr '[:upper:]' '[:lower:]'`
+FLASK_IMAGE_NAME=`echo "${BUILD_REPO_NAME}_${TRAVIS_BRANCH}_${FLASK_CONTAINER_SERVICE_NAME}" | tr '[:upper:]' '[:lower:]'`
+
 PACKAGE_NAME=`jq '.name' version.json | tr -d '"'` 
 PACKAGE_VERSION=`jq '.version' version.json | tr -d '"'`
-echo "Package name : ${PACKAGE_NAME}"
-jq --arg froalaeditor "file:${PACKAGE_NAME}-${PACKAGE_VERSION}.tgz" '.dependencies["froala-editor"] |= $froalaeditor' package.json  > new.file && cat new.file > package.json && rm -f new.file
-echo "verify package"
-cat package.json
-docker build -t  ${IMAGE_NAME}:${SHORT_COMMIT} --build-arg PackageName=${PACKAGE_NAME} --build-arg PackageVersion=${PACKAGE_VERSION} --build-arg NexusUser=${NEXUS_USER} --build-arg NexusPassword=${NEXUS_USER_PWD} .
+echo "Package name & version : ${PACKAGE_NAME} $PACKAGE_VERSION "
+#
+# Update package.json with wright froala editor name & version
+#
+#  no package.json in this repo
+#jq --arg froalaeditor "file:${PACKAGE_NAME}-${PACKAGE_VERSION}.tgz" '.dependencies["froala-editor"] |= $froalaeditor' package.json  > new.file && cat new.file > package.json && rm -f new.file
+#echo "verify package"
+#cat package.json
+
+docker build -t  ${DJANGO_IMAGE_NAME}:${SHORT_COMMIT} --build-arg PackageName=${PACKAGE_NAME} --build-arg PackageVersion=${PACKAGE_VERSION} --build-arg NexusUser=${NEXUS_USER} --build-arg NexusPassword=${NEXUS_USER_PWD} -f Dockerfile_django .
 sleep 3
 docker image ls 
-echo "uploading to nexus ${PACKAGE_NAME}, if not a new PR"
+#
+#  don't upload for new PR
+#
 if [ ${TRAVIS_PULL_REQUEST} != "false" ];  then echo "Not publishing a pull request !!!" && exit 0; fi
+
+echo "uploading to DJANGO_IMAGE_NAME nexus" ${PACKAGE_NAME}
+
 docker login -u ${NEXUS_USER} -p ${NEXUS_USER_PWD} ${NEXUS_CR_TOOLS_URL}
-docker tag  ${IMAGE_NAME}:${SHORT_COMMIT} ${NEXUS_CR_TOOLS_URL}/froala-${IMAGE_NAME}:${PACKAGE_VERSION}
-docker push ${NEXUS_CR_TOOLS_URL}/froala-${IMAGE_NAME}:${PACKAGE_VERSION}
+docker tag  ${DJANGO_IMAGE_NAME}:${SHORT_COMMIT} ${NEXUS_CR_TOOLS_URL}/froala-${DJANGO_IMAGE_NAME}:${PACKAGE_VERSION}
+docker push ${NEXUS_CR_TOOLS_URL}/froala-${DJANGO_IMAGE_NAME}:${PACKAGE_VERSION}
+
+docker build -t  ${FLASK_IMAGE_NAME}:${SHORT_COMMIT} --build-arg PackageName=${PACKAGE_NAME} --build-arg PackageVersion=${PACKAGE_VERSION} --build-arg NexusUser=${NEXUS_USER} --build-arg NexusPassword=${NEXUS_USER_PWD} -f Dockerfile_django .
+sleep 3
+docker image ls 
+echo "uploading to FLASK_IMAGE_NAME nexus" ${PACKAGE_NAME}
+
+docker tag  ${FLASK_IMAGE_NAME}:${SHORT_COMMIT} ${NEXUS_CR_TOOLS_URL}/froala-${FLASK_IMAGE_NAME}:${PACKAGE_VERSION}
+docker push ${NEXUS_CR_TOOLS_URL}/froala-${FLASK_IMAGE_NAME}:${PACKAGE_VERSION}
+
+
+
